e25102ca21cfeb2c868d18f13d3495808e331e5f
BugHunter-X <askzenai@gmail.com>
2025-02-11 00:42:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
